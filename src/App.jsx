3afba76f008533d3f642596f4a99c3da58f3e8d8
siddu0426 <msvssiddhartha@gmail.com>
2025-10-13 14:02:17 +0530

    created react application

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './App.css';
+import Hero from './pages/Hero';
+
+function App(){
+  return(
+    <>
+    <h1>Hello, World!</h1>,
+    <Hero />
+    
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/Hero.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Hero(){
+  return(
+    <>
+    <h2>This is the Hero Component</h2>
+    </>
+  )
+}   
+
+export default Hero;    
