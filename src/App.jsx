ff53911a23bb27964cdfb40fcf5566c30c5583d1
siddu0426 <msvssiddhartha@gmail.com>
2025-10-14 23:17:25 +0530

    installed router dom

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,30 @@ import './App.css';
 import Hero from './pages/Hero';
 import HeroCarousel from './components/HeroCarousel';
 import Navbar from "./components/Navbar";
+import { Routes, Route } from "react-router-dom";
+import TopWearPage from "./pages/TopWearPage";
+import BottomwearPage from "./pages/BottomwearPage";
+import AthleisurePage from "./pages/AthleisurePage";
 
 function App(){
   return(
     <>
-    <Navbar />
-    <HeroCarousel /> 
-    <Hero />
+    <div className="App">
+
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={
+          <>
+          <HeroCarousel />
+          <Hero />
+          </>
+      } />
+        <Route path="/topwear" element={<TopWearPage />} />
+        <Route path="/bottomwear" element={<BottomwearPage />} />
+        <Route path="/athleisure" element={<AthleisurePage />} />
+      </Routes>
+    </div>
     </>
   );
 }
