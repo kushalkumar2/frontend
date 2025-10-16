b0a7175fbf0b4362a5b87b1b0be1e4693d5b680d
siddu0426 <msvssiddhartha@gmail.com>
2025-10-16 22:16:32 +0530

    created combo of all product data

---
+++ src/data/allProducts.jsx
@@ -0,0 +1,22 @@
+import { topwearData } from './topwearData';
+import { bottomwearData } from './bottomwearData';
+import { athleisureData } from './athleisureData';
+import { trendingData } from './trendingData';
+
+// Combine all arrays into one
+const combinedData = [
+  ...topwearData,
+  ...bottomwearData,
+  ...athleisureData,
+  ...trendingData,
+];
+
+// Use a Map to automatically handle duplicates, keeping the first one seen
+const uniqueProductsMap = new Map();
+combinedData.forEach(product => {
+  if (!uniqueProductsMap.has(product.id)) {
+    uniqueProductsMap.set(product.id, product);
+  }
+});
+
+export const allProducts = Array.from(uniqueProductsMap.values());
