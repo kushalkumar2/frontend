94302710a5cf7cd88bbb64efd4bf52e054bc8126
siddu0426 <msvssiddhartha@gmail.com>
2025-10-16 08:23:15 +0530

    mock data for display

---
+++ src/data/athleisureData.js
@@ -0,0 +1,6 @@
+export const athleisureData = [
+  { id: 'ath1', image: 'https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcTx7rCmMn4njiFgXIMseRKTQdiZNXlvWfBYsSxrsHPk-qhU8XMALMDVeEsr_4holE26biCg_ON67qaSaJRcOORpD83LTB5tgc-gRXbCvZyffWJPoh-Sre3-Gi6H-jqK1GZ0F2U6rcs&usqp=CAc', title: 'Comfort Grey Joggers', price: 1599 },
+  { id: 'ath2', image: 'https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcTmbQaw2w9eLcdhcpStKuOu-WNfih_oN1T5VaK5fX1AzFeLEvpilyQVTr6nMV6pB_lIbnNhWKhWV-jBfrti87SPKj6-XA6r5Cg8SmbVnrWo_TVnLXhFND8x_Q&usqp=CAc', title: 'Performance Shorts', price: 899 },
+  { id: 'ath3', image: 'https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcQc6ojZUPfnaFLO9_lCWbZO8xyYpNPzGAZGMRplGDGDqjDYTzRb65xn77Rz0yP3FUGZ6dstWg82hfEbZDzpgzX4JX9QIG-7ivm4zJZJhDz53Xy1AKiSQpa1a_wlm3ZbBJiwBB5ASiY&usqp=CAc', title: 'Training Tank Top', price: 599 },
+  { id: 'ath4', image: 'https://images.unsplash.com/photo-1515886657613-9f3515b0c78f?q=80&w=1920', title: 'Urban Athleisure Set', price: 2999 },
+];
---
+++ src/data/topwearData.js
@@ -0,0 +1,8 @@
+export const topwearData = [
+  { id: 'top1', image: 'https://images.unsplash.com/photo-1583743814966-8936f5b7be1a?q=80&w=1974', title: 'Classic Black Tee', price: 799 },
+  { id: 'top2', image: 'https://images.unsplash.com/photo-1576566588028-4147f3842f27?q=80&w=1964', title: 'Striped Crewneck', price: 1199 },
+  { id: 'top3', image: 'https://image.hm.com/assets/hm/59/8f/598fa0c8560b84976d38240e6e1c2c0ccae792e1.jpg?imwidth=1260', title: 'White Polo Shirt', price: 1499 },
+  { id: 'top4', image: 'https://images.unsplash.com/photo-1521572163474-6864f9cf17ab?q=80&w=1974', title: 'Plain White Tee', price: 699 },
+  { id: 'top5', image: 'https://images-cdn.ubuy.co.in/665aaed151378f161d43a7c6-oversized-graphic-tshirt-for-men-letter.jpg', title: 'Graphic Print Tee', price: 999 },
+  { id: 'top6', image: 'https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcTgVj-WDJarT5A20Edbxrt7lBnjVv5de1_ZSD86MwZe6LT-jxTPMgMj5EjMVxmY-r67PfRE57EMwMk4qD2DUsuhdNgoe1bHEHtfEC_pj27mbSnYroTyyDPQgQ', title: 'Linen Button-Up', price: 1899 },
+];
